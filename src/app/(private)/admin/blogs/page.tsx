4fa4e74255ca323de15f93830e5b63b7d43850c3
Clifford William <ccliffordwilliam@gmail.com>
2024-02-12 03:16:23 +0800

    feat(admin-blogs): admin owned blogs data table

--- src/app/(private)/admin/blogs/page.tsx
+++ src/app/(private)/admin/blogs/page.tsx
@@ -1,13 +1,25 @@
+import { BlogsColumns } from "@/components/BlogColumns";
+import { DataTable } from "@/components/DataTable";
 import { Button } from "@/components/ui/button";
+import IsUserAdmin from "@/lib/IsUserAdmin";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
-const Page = () => {
+const Page = async () => {
+  const { userId } = await auth();
+  if (!userId || !IsUserAdmin(userId)) {
+    redirect("/sign-in");
+  }
+  const blogs = await db.blog.findMany({ orderBy: { createdAt: "desc" } });
   return (
     <>
-      <div>
+      <div className="p-6">
         <Link href={"/admin/createBlog"}>
           <Button>Add blog</Button>
         </Link>
+        <DataTable columns={BlogsColumns} data={blogs} />
       </div>
     </>
   );
---
+++ src/components/BlogColumns.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { Blog } from "@prisma/client";
+import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown, Pencil } from "lucide-react";
+import { Button } from "./ui/button";
+import { Badge } from "./ui/badge";
+import { cn } from "@/lib/utils";
+
+export const BlogsColumns: ColumnDef<Blog>[] = [
+  {
+    accessorKey: "title",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant={"ghost"}
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          <span>Title</span>
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: "isPublished",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant={"ghost"}
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          <span>Published</span>
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      const { id } = row.original;
+      const isPublished = row.getValue("isPublished") || false;
+      return (
+        <Badge className={cn("bg-slate-500", isPublished && "bg-sky-700")}>
+          {isPublished ? "Published" : "Draft"}
+        </Badge>
+      );
+    },
+  },
+  {
+    accessorKey: "action",
+    header: "Action",
+    cell: ({ row }) => {
+      const { id } = row.original;
+      return (
+        <Button variant={"ghost"}>
+          <Pencil className="mr-2 h-4 w-4" />
+          <span>Edit</span>
+        </Button>
+      );
+    },
+  },
+];
